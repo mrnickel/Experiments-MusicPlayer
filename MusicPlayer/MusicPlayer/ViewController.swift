726908e6411fe3b7d65ef2a011c126584a4e4282
Ryan Nickel <ryan@ryannickel.com>
2016-01-05 11:32:32 -0500

    Initial commit

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Ryan Nickel on 2016-01-05.
+//  Copyright © 2016 Ryan Nickel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
